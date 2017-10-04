868f8cc2e9498b3454c8f873a9863c13574a9708
jlameira <jlameira@gmail.com>
2017-10-03 21:44:45 -0300

    server port process.ENV

--- src/config/server.js
+++ src/config/server.js
@@ -5,15 +5,20 @@ var MongoStore = require('connect-mongo/es5')(session);
 var secret = require('./secret');
 var allowCors = require('./cors');
 const server = express();
-const port = secret.port;
-server.use(bodyParser.urlencoded({ extended: true }));
+const port = process.env.PORT || secret.port;
+server.use(bodyParser.urlencoded({
+    extended: true
+}));
 server.use(bodyParser.json());
 server.use(allowCors);
 server.use(session({
     resave: true,
     saveUninitialized: true,
     secret: secret.authSecret,
-    store: new MongoStore({ url: secret.database, autoReconnect: true })
+    store: new MongoStore({
+        url: secret.database,
+        autoReconnect: true
+    })
 }));
 server.listen(port, function () {
     console.log(`BACKEND is running on port ${port}.`);
